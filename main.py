89450abb7c9b6cfe25f70dfc72c968c8a0282389
saad07-x <k190133@nu.edu.pk>
2023-09-23 01:34:22 +0500

    Final Version

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import mysql.connector
 import os
 import shutil
 import re
+added_question_ids = set()
 
-# Move database configuration to a separate file or use environment variables
 DB_CONFIG = {
     'host': "127.0.0.1",
     'user': "root",
@@ -14,7 +14,6 @@ DB_CONFIG = {
 def setup_database():
     try:
         conn = mysql.connector.connect(**DB_CONFIG)
-
         with conn.cursor() as cursor:
             cursor.execute('''
                 CREATE TABLE IF NOT EXISTS papers (
@@ -23,7 +22,6 @@ def setup_database():
                     total_marks INT
                 )
             ''')
-
             cursor.execute('''
                 CREATE TABLE IF NOT EXISTS questions (
                     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -34,7 +32,6 @@ def setup_database():
                     question_marks INT
                 )
             ''')
-
         conn.commit()
     except Exception as e:
         raise e
@@ -44,52 +41,44 @@ def setup_database():
 def get_question_info(difficulty, topic, marks):
     try:
         conn = mysql.connector.connect(**DB_CONFIG)
-
         with conn.cursor() as cursor:
             cursor.execute('''
-                SELECT questions.question_path, papers.paper_name
+                SELECT questions.question_marks, questions.question_path, papers.paper_name
                 FROM questions
                 INNER JOIN papers ON questions.paper_id = papers.id
                 WHERE questions.question_difficulty = %s
                 AND questions.topic_name = %s
-                AND questions.question_marks = %s
+                AND questions.question_marks <= %s
             ''', (difficulty, topic, marks))
-
             result = cursor.fetchone()
-
         if result:
-            question_path, paper_name = result
-            return question_path, paper_name
+            question_marks, question_path, paper_name = result
+            return question_marks, question_path, paper_name
         else:
             return None
     except Exception as e:
         raise e
     finally:
         conn.close()
-        
 
 def create_folder_in_generated(name):
     generated_folder = 'generated'
     new_folder_path = os.path.join(generated_folder, name)
-
     try:
         os.mkdir(new_folder_path)
         print(f"Folder '{name}' created successfully inside 'generated'.")
-        return new_folder_path  # Return the new folder path
+        return new_folder_path
     except FileExistsError:
         print(f"Folder '{name}' already exists inside 'generated'.")
-        return new_folder_path  # Return the existing folder path
+        return new_folder_path
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-        return None  # Return None in case of an error
+        return None
 
-        
 def copy_folder(source_path, destination_path, question_number):
     try:
         shutil.copytree(source_path, destination_path)
         print(f"Folder copied from '{source_path}' to '{destination_path}' successfully.")
-
-        # Rename .tex files to 'quiz.tex' inside the destination folder
         for root, _, files in os.walk(destination_path):
             for file in files:
                 if file.endswith('.tex'):
@@ -102,15 +91,11 @@ def copy_folder(source_path, destination_path, question_number):
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
-
 def copy_files_to_folder(source_folder, destination_folder):
-    # Define the source path for the four files
     source_files = ['logo.JPG', 'logo.png', 'logo2.png', 'main.tex', 'packages.tex', 'title.tex', 'title2.tex']
-
     for file in source_files:
         source_file_path = os.path.join(source_folder, file)
         destination_file_path = os.path.join(destination_folder, file)
-
         try:
             shutil.copy2(source_file_path, destination_file_path)
             print(f"File '{file}' copied to '{destination_folder}' successfully.")
@@ -121,87 +106,73 @@ def replace_quiz_number(tex_file_path, new_quiz_number):
     try:
         with open(tex_file_path, 'r') as file:
             content = file.read()
-
-        # Use regular expression to find and replace the Quiz number
         pattern = r'(\\includegraphics\[.*\]{Questions/quiz )(\d+)(/images/.*?})'
         modified_content = re.sub(pattern, fr'\\includegraphics[{{scale=0.4}}]{{Questions/quiz {new_quiz_number}\3', content)
-
-        # Save the modified content back to the file
         with open(tex_file_path, 'w') as file:
             file.write(modified_content)
-
         print(f"Modified Quiz number in '{tex_file_path}'.")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-        
-        
+
 def update_main_tex(main_tex_path, tex_file_relative_path):
     tex_file_relative_path = tex_file_relative_path.replace("\\","/")
+    print(tex_file_relative_path)
     try:
         with open(main_tex_path, 'r') as main_tex_file:
             content = main_tex_file.read()
-
-        # Use regular expression to find the location where the new \include{} line should be added
         pattern = r'\\begin{questions}([\s\S]*?)\\end{questions}'
         match = re.search(pattern, content)
-
         if match:
-            # Extract the content inside \begin{questions} and \end{questions}
             questions_content = match.group(1)
-
-            # Generate the new \include{} line
             new_include_line = fr'\include{{{tex_file_relative_path}}}'
-            
-            # Append the new \include{} line to the questions_content
             updated_content = content.replace(match.group(0), f'\\begin{{questions}}{questions_content}{new_include_line}\n\\end{{questions}}')
-
-            # Save the updated content back to the main.tex file
             with open(main_tex_path, 'w') as main_tex_file:
                 main_tex_file.write(updated_content)
-
             print(f"Updated '{main_tex_path}' to include '{tex_file_relative_path}'.")
         else:
             print(f"Error: Unable to find the location to insert '{tex_file_relative_path}' in '{main_tex_path}'.")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-                
-if __name__ == "__main__":
-    # Example usage:
-    folder_name = 'test79'
-    new_paper_folder = create_folder_in_generated(folder_name)
-    copy_files_to_folder('Z:\Codes\PaperCraft\data\common', new_paper_folder)  # Replace with the actual source path
 
-    num_questions = 3  # Set the number of questions you want to fetch
+if __name__ == "__main__":
+    
+    folder_name = input("Enter New Paper Name: " )
+    new_paper_folder = create_folder_in_generated(folder_name)
+    copy_files_to_folder('common', new_paper_folder)
+    total_marks = int(input("Enter Total Marks: "))
+    num_questions = 2
+    current_total_marks = 0
 
     for question in range(num_questions):
-        question_difficulty = input("Enter question difficulty: ") # hard
-        topic_name = input("Enter topic name: ") # algebra
-        question_marks = int(input("Enter question marks: ")) # 10
-
-        path = os.path.join('generated', folder_name)
-        print(path)
-        tex_file_relative_path = os.path.join('Questions', f'quiz {question + 1}', f'quiz {question + 1}.tex')
-
-        try:
-            setup_database()
-            question_info = get_question_info(question_difficulty, topic_name, question_marks)
-            
-            if question_info:
-                question_path, paper_name = question_info
-                print(f"Question Path: {question_path}")
-                print(f"New paper Path: {path}")
-                copy_folder(question_path, os.path.join(path, 'Questions', 'quiz ' + str(question + 1)), question)
-                # Example usage:
-                tex_file_path =  path + '\Questions' + '\quiz ' + str(question + 1) + '\quiz' + str(question + 1) + '.tex' # Replace with the actual path to your .tex file
-                print("hi")
-                print (tex_file_path)
-                new_quiz_number = str(question + 1)  # Replace with the new Quiz number
-                replace_quiz_number(tex_file_path, new_quiz_number)
-                # Update the main.tex file to include the new question
-                main_tex_path = os.path.join(path, 'main.tex')
-                update_main_tex(main_tex_path, tex_file_relative_path)
-
-            else:
-                print("No matching question found.")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+        question_difficulty = input("Enter question difficulty: ")
+        topic_name = input("Enter topic name: ")
+        
+        # Remove the input prompt for question_marks
+        # question_marks = int(input("Enter question marks: "))
+        
+        if current_total_marks <= total_marks:
+            try:
+                setup_database()
+                # Fetch question_marks from the database
+                question_marks, question_path, paper_name = get_question_info(question_difficulty, topic_name, total_marks - current_total_marks)
+                if question_path and question_marks is not None:
+                    if question_path not in added_question_ids:  # Check if the question hasn't been added before
+                        added_question_ids.add(question_path)
+                        print(f"Question Path: {question_path}")
+                        print(f"New paper Path: {new_paper_folder}")
+                        copy_folder(question_path, os.path.join(new_paper_folder, 'Questions', 'quiz ' + str(question + 1)), question)
+                        tex_file_path = os.path.join(new_paper_folder, 'Questions', 'quiz ' + str(question + 1), f'quiz{question + 1}.tex')
+                        new_quiz_number = str(question + 1)
+                        replace_quiz_number(tex_file_path, new_quiz_number)
+                        main_tex_path = os.path.join(new_paper_folder, 'main.tex')
+                        tex_file_relative_path = os.path.join('Questions', f'quiz {question + 1}', f'quiz{question + 1}.tex')
+                        update_main_tex(main_tex_path, tex_file_relative_path) 
+                        current_total_marks += question_marks
+                    else:
+                        print("This question has already been added to the paper. Skipping.")
+                else:
+                    print("No matching question found.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+        else:
+            print("Adding this question would exceed the total marks. Please re-enter.")
--- tag.py
+++ tag.py
@@ -2,7 +2,6 @@ import mysql.connector
 import os
 import shutil
 
-# Define your MySQL credentials here
 db_config = {
     "host": "127.0.0.1",
     "user": "root",
@@ -10,47 +9,44 @@ db_config = {
     "database": "mysql"
 }
 
-# Function to check if a paper exists in the database
 def paper_exists(paper_name, cursor):
     cursor.execute('SELECT id FROM papers WHERE paper_name=%s', (paper_name,))
     return cursor.fetchone() is not None
 
-# Function to create a new paper entry in the database
 def create_paper_entry(paper_name, total_marks, cursor):
     cursor.execute('INSERT INTO papers (paper_name, total_marks) VALUES (%s, %s)', (paper_name, total_marks))
 
-# Function to add a question to the database
-def add_question_to_db(paper_id, difficulty, topic, marks, question_path, cursor):
-    cursor.execute('INSERT INTO questions (paper_id, question_path, question_difficulty, topic_name, question_marks) VALUES (%s, %s, %s, %s, %s)',
-                   (paper_id, question_path, difficulty, topic, marks))
+def question_exists(question_path, cursor):
+    cursor.execute('SELECT id FROM questions WHERE question_path=%s', (question_path,))
+    return cursor.fetchone() is not None
 
-# Function to process a paper and its questions
+def add_question_to_db(paper_id, difficulty, topic, marks, question_path, cursor):
+    if not question_exists(question_path, cursor):
+        cursor.execute('INSERT INTO questions (paper_id, question_path, question_difficulty, topic_name, question_marks) VALUES (%s, %s, %s, %s, %s)',
+                       (paper_id, question_path, difficulty, topic, marks))
+    else:
+        print(f"Question with path '{question_path}' already exists. Skipping insertion.")
+
 def process_paper(paper_path, cursor):
     paper_name = os.path.basename(paper_path)
     
-    # Check if the paper already exists in the database
     if not paper_exists(paper_name, cursor):
         total_marks = int(input(f"Enter total marks for {paper_name}: "))
         create_paper_entry(paper_name, total_marks, cursor)
     
-    # Process questions in the 'Questions' folder
     questions_path = os.path.join(paper_path, 'Questions')
     for question_folder in os.listdir(questions_path):
         question_path = os.path.join(questions_path, question_folder)
-        print(question_path)
         if os.path.isdir(question_path):
             difficulty = input(f"Enter difficulty for {question_folder}: ")
             topic = input(f"Enter topic for {question_folder}: ")
             marks = int(input(f"Enter marks for {question_folder}: "))
             
-            # Get the paper_id for the current paper
             cursor.execute('SELECT id FROM papers WHERE paper_name=%s', (paper_name,))
             paper_id = cursor.fetchone()[0]
             
-            # Add the question to the database
             add_question_to_db(paper_id, difficulty, topic, marks, question_path, cursor)
 
-# Main function to process question papers in the 'data' folder
 def process_question_papers():
     try:
         conn = mysql.connector.connect(**db_config)
@@ -58,11 +54,8 @@ def process_question_papers():
 
         data_folder = 'data'
         for paper_name in os.listdir(data_folder):
-            print(paper_name)
             paper_path = os.path.join(data_folder, paper_name)
-            print(paper_path)
             if os.path.isdir(paper_path):
-                print(paper_path)
                 process_paper(paper_path, cursor)
         
         conn.commit()
